9ea374603c85c1c4fc27d8018386295b621f31a3
Boris Stojanovic <bstojanovic3518rn@raf.rs>
2021-01-29 06:40:07 +0100

    Dodao artikle i seed-ovao bazu

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -3,20 +3,29 @@
 // a relevant structure within app/javascript and only use these pack files to reference
 // that code so it'll be compiled.
 
+//rails and storage
 import Rails from "@rails/ujs"
 Rails.start()
 import Turbolinks from "turbolinks"
 Turbolinks.start()
 import * as ActiveStorage from "@rails/activestorage"
 ActiveStorage.start()
+
+//css and icons
 import "channels"
 import "bootstrap"
 import "@fortawesome/fontawesome-free"
+import { library, dom } from '@fortawesome/fontawesome-svg-core'
+import { fas } from '@fortawesome/free-solid-svg-icons'
+import { faTwitter } from '@fortawesome/free-brands-svg-icons'
+library.add(fas, faTwitter)
 import "./src/application.scss"
+
+//ratings
 require("/app/javascript/packs/raty")
 require('jquery')
 
-
+//jquery won't work otherwise - why?
 global.$ = jQuery;
 require("trix")
 require("@rails/actiontext")
